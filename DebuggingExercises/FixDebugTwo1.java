e43852f24e63d2395294a26c44f337c4fb7732e2
NangSengKham <nangsengkham25@gmail.com>
2020-11-23 00:29:19 +0800

    FixDebug

---
+++ DebuggingExercises/FixDebugTwo1.java
@@ -0,0 +1,16 @@
+public class FixDebugTwo1
+{
+   public static void main(String[] args)
+   {
+      int oneInt = 315;
+      double oneDouble = 12.4;
+      char oneChar = 'A';
+      System.out.print("The int is ");
+      System.out.println(" The oneInt is ");
+      System.out.print("The double is ");
+      System.out.println(oneDouble);
+      System.out.print("The char is ");
+      System.out.println(oneChar);
+   }
+}
+
